0d91adbb71151d0672279482d612f9ceb7a1de00
Roelof Wobben <Roelof Wobben>
2016-12-08 15:29:19 +0100

    solved 500 error

---
+++ profiles.clj
@@ -0,0 +1,7 @@
+;; WARNING
+;; The profiles.clj file is used for local environment variables, such as database credentials.
+;; This file is listed in .gitignore and will be excluded from version control by Git.
+
+{:profiles/dev  {:env {:key "14OGzuak"}}
+ :profiles/test {:env {}}
+ }
