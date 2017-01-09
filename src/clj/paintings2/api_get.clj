3225374ca492cd3886588801ac2ab570e2a868da
Roelof Wobben <Roelof Wobben>
2017-01-09 14:34:17 +0100

    Wierd error message on testing the fetch function

--- src/clj/paintings2/api_get.clj
+++ src/clj/paintings2/api_get.clj
@@ -90,13 +90,17 @@
  :args (s/cat :response :image/response)
  :ret  :image/object)
 
+(s/fdef fetch-paintings-and-images-front-page
+ :args (s/cat :args (s/coll-of ::objectNumber))
+ :ret (s/coll-of (s/and :basic/artObject :image/object)))
+
 ; the test functions
 
 ;(stest/summarize-results (stest/check 'paintings2.api-get/get-objectNumbers))
 ;(stest/summarize-results (stest/check 'paintings2.api-get/get-art-object))
 ;(stest/summarize-results (stest/check 'paintings2.api-get/get-data-detail-page))
 ;(stest/summarize-results (stest/check 'paintings2.api-get/get-data-front-page))
-;(stest/summarize-results (stest/check 'paintings2.api-get/get-image-url'))
+;(stest/summarize-results (stest/check `paintings2.api-get/get-image-url))
 
 ;  the core functions
 
@@ -173,5 +177,5 @@
 (defn fetch-paintings-and-images-detail-page
   [ids]
   (let [paintings (pmap (comp get-data-detail-page get-art-object read-json-data) ids)
-        images (pmap (comp get-image-url read-image-data) ids)]
-    (mapv merge paintings images)))
+        images (pmap (comp get-image-url read-image-data) ids)])
+  (mapv merge paintings images))
